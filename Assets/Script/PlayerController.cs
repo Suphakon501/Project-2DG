ccb5d5448b93ac64b462f4bbab2f03c139c3dc03
Suphakon501 <ghostsad0015@gmail.com>
2026-01-19 16:17:27 +0700

    Revert "Player"
    This reverts commit ca2fa32c53c19c37ac10fc5955855021cf89f97a.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,39 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement Settings")]
-    public float maxRunSpeed = 10f;      
-    public float acceleration = 2f;      
-    [SerializeField]private float currentRunSpeed = 0f;   
+    public float runSpeed = 5f;
 
-    [Header("Gravity Settings")]
-    public float gravityScale = 8f;      
-    private Rigidbody2D rb;
-    private bool gravityDown = true;
-
-    [Header("Ground Check")]
-    public Transform groundCheck;        
-    public float checkRadius = 0.3f;     
-    public LayerMask whatIsGround;       
-    private bool isGrounded;
+    Rigidbody2D rb;
+    bool gravityDown = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = gravityScale;
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     void Update()
     {
         
-        currentRunSpeed = Mathf.MoveTowards(currentRunSpeed, maxRunSpeed, acceleration * Time.deltaTime);
+        rb.linearVelocity = new Vector2(runSpeed, rb.linearVelocity.y);
 
-        rb.linearVelocity = new Vector2(currentRunSpeed, rb.linearVelocity.y);
-
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             FlipGravity();
         }
@@ -43,13 +26,12 @@ public class PlayerController : MonoBehaviour
     void FlipGravity()
     {
         gravityDown = !gravityDown;
-        rb.gravityScale = gravityDown ? gravityScale : -gravityScale;
 
-        transform.Rotate(180f, 0f, 0f);
+        rb.gravityScale *= -1;
 
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
-        float pushDirection = gravityDown ? -1f : 1f;
-
-        rb.AddForce(Vector2.up * pushDirection * 2f, ForceMode2D.Impulse);
+        
+        Vector3 scale = transform.localScale;
+        scale.y *= -1;
+        transform.localScale = scale;
     }
 }
